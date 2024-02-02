11c52f09ada50a20497cb863d05dd98e38b24ba6
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2024-02-03 03:44:06 +0530

    fix adheeraj copyright icon and added link to it

--- src/assets/SideBar.jsx
+++ src/assets/SideBar.jsx
@@ -6,17 +6,30 @@ import Parallax from "./Parallax";
 const SideBar = () => {
   return (
     <>
-      <div className="flex flex-col fixed content-start h-screen bg-primary text-secondary w-16 justify-items-center  top-0 left-0 shadow-lg z-20">
-        <SideBarIcon icon={<IoMdHome size="28" />} text={"Home"} />
+      <div className="font-coolvetica flex flex-col fixed content-start h-screen bg-primary text-secondary w-16 justify-items-center  top-0 left-0 shadow-lg z-20">
+        <SideBarIcon flink={"/"} icon={<IoMdHome size="28" />} text={"Home"} />
         <hr className="sidebar-hr" />
-        <SideBarIcon icon={<FaDharmachakra size="28" />} text={"Projects"} />
-        <SideBarIcon icon={<PiNutBold size="28" />} text={"Services"} />
+        <SideBarIcon
+          flink={"/"}
+          icon={<FaDharmachakra size="28" />}
+          text={"Projects"}
+        />
+        <SideBarIcon
+          flink={"/"}
+          icon={<PiNutBold size="28" />}
+          text={"Services"}
+        />
         <hr className="sidebar-hr" />
-        <SideBarIcon icon={<TbListDetails size="28" />} text={"About"} />
+        <SideBarIcon
+          flink={"/"}
+          icon={<TbListDetails size="28" />}
+          text={"About"}
+        />
         <SideBarIcon
           icon={<FaRegCopyright size="28" />}
           text={"Adheera"}
           cop={"cop"}
+          flink={"https://adheera.netlify.com"}
         />
       </div>
       <Parallax />
@@ -24,12 +37,14 @@ const SideBar = () => {
   );
 };
 
-const SideBarIcon = ({ icon, text, cop }) => (
-  <div className={`sidebar-icon group ${cop} `}>
-    {icon}
-    <span className="sidebar-text group-hover:scale-100 group-hover:z-50 ">
-      {text}
-    </span>
-  </div>
+const SideBarIcon = ({ icon, text, cop, flink }) => (
+  <a href={flink}>
+    <div className={`sidebar-icon group ${cop} `}>
+      {icon}
+      <span className="sidebar-text group-hover:scale-100 group-hover:z-50 ">
+        {text}
+      </span>
+    </div>
+  </a>
 );
 export default SideBar;
--- src/assets/WelcomePage.jsx
+++ src/assets/WelcomePage.jsx
@@ -4,9 +4,9 @@ const WelcomePage = () => {
   return (
     <>
       <section>
-        <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8 ">
           <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
-            <div className="relative z-10 lg:py-16">
+            <div className="relative z-10 lg:py-16 ">
               <div className="relative h-64 sm:h-80 lg:h-full">
                 <img
                   alt="House"
@@ -16,7 +16,7 @@ const WelcomePage = () => {
               </div>
             </div>
 
-            <div className=" relative flex items-center bg-gray-100">
+            <div className=" relative flex items-center bg-gray-100 ">
               <span className="hidden lg:absolute lg:inset-y-0 lg:-start-16 lg:block lg:w-16 lg:bg-gray-100"></span>
 
               <div className="p-8 sm:p-16 lg:p-24">
