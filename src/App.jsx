440a2c6efc38ef80972f49e9f12fcecdb1189d15
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2024-02-05 20:13:50 +0530

    solved netlify error added some adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ function App() {
     <>
       <BrowserRouter>
         <>
-          <SideBar />
-          <Routes>
-            <Route index path="/" element={<Home />}></Route>
-            <Route path="/About" element={<About />}></Route>
-          </Routes>
+          <div className="flex">
+            <SideBar />
+            <Routes>
+              <Route index path="/" element={<Home />}></Route>
+              <Route path="/About" element={<About />}></Route>
+            </Routes>
+          </div>
         </>
       </BrowserRouter>
     </>
