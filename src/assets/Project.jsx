6a1b346e9eb3890568efb676ac6ba14aefbe5703
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2024-02-16 00:47:39 +0530

    finally project all setting done only need to put values

--- src/assets/Project.jsx
+++ src/assets/Project.jsx
@@ -98,46 +98,48 @@ const Project = () => {
         </li>
       </ul>
       <nav className="nav">
-        <button
-          type="button"
-          className="btn prev mt-8 inline-block font-coolvetica rounded border border-green-600 bg-green-500  py-3 text-sm font-medium text-black hover:bg-transparent hover:text-white tracking-widest focus:outline-none focus:ring active:text-white"
-        >
-          <div className="flex flex-row align-middle">
-            <svg
-              className="w-5 mr-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <p className="ml-2">Prev</p>
-          </div>
-        </button>
-        <button
-          type="button"
-          className="btn  next mt-8 inline-block font-coolvetica rounded border border-green-600 bg-green-500  py-3 text-sm font-medium text-black hover:bg-transparent hover:text-white tracking-widest focus:outline-none focus:ring active:text-white"
-        >
-          <div className="flex flex-row align-middle">
-            <span className="mr-2">Next</span>
-            <svg
-              className="w-5 ml-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </div>
-        </button>
+        {/* <button */}
+        {/*   type="button" */}
+        {/*   className="btn prev mt-8 inline-block font-coolvetica rounded border border-green-600 bg-green-500  py-3 text-sm font-medium text-black hover:bg-transparent hover:text-white tracking-widest " */}
+        {/* > */}
+        {/*   <div className="flex flex-row align-middle"> */}
+        {/*     <svg */}
+        {/*       className="w-5 mr-2" */}
+        {/*       fill="currentColor" */}
+        {/*       viewBox="0 0 20 20" */}
+        {/*       xmlns="http://www.w3.org/2000/svg" */}
+        {/*     > */}
+        {/*       <path */}
+        {/*         fillRule="evenodd" */}
+        {/*         d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" */}
+        {/*         clipRule="evenodd" */}
+        {/*       ></path> */}
+        {/*     </svg> */}
+        {/*     <p className="ml-2">Prev</p> */}
+        {/*   </div> */}
+        {/* </button> */}
+        {/* <button */}
+        {/*   type="button" */}
+        {/*   className="btn  next mt-8 inline-block font-coolvetica rounded border border-green-600 bg-green-500  py-3 text-sm font-medium text-black hover:bg-transparent hover:text-white tracking-widest " */}
+        {/* > */}
+        {/*   <div className="flex flex-row align-middle"> */}
+        {/*     <span className="mr-2">Next</span> */}
+        {/*     <svg */}
+        {/*       className="w-5 ml-2" */}
+        {/*       fill="currentColor" */}
+        {/*       viewBox="0 0 20 20" */}
+        {/*       xmlns="http://www.w3.org/2000/svg" */}
+        {/*     > */}
+        {/*       <path */}
+        {/*         fillRule="evenodd" */}
+        {/*         d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" */}
+        {/*         clipRule="evenodd" */}
+        {/*       ></path> */}
+        {/*     </svg> */}
+        {/*   </div> */}
+        {/* </button> */}
+        <ion-icon class="btn prev" name="arrow-back-outline"></ion-icon>
+        <ion-icon class="btn next" name="arrow-forward-outline"></ion-icon>
       </nav>
     </>
   );
