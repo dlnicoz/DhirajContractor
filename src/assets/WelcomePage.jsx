1d2fe968630eb04e5a10713b036902c3e93e8bcc
dlnicoz <92986739+dlnicoz@users.noreply.github.com>
2024-03-01 20:41:20 +0530

    css resposiveness

--- src/assets/WelcomePage.jsx
+++ src/assets/WelcomePage.jsx
@@ -3,12 +3,13 @@ import ConfigData from "../config.json";
 
 const WelcomePage = () => {
   const { info } = ConfigData;
-  const Founder = "https://raw.githubusercontent.com/dlnicoz/DhirajContractor/master/src/assets/founder.webp"
+  const Founder =
+    "https://raw.githubusercontent.com/dlnicoz/DhirajContractor/master/src/assets/founder.webp";
   return (
     <>
       <section>
         <div className="mx-auto max-w-screen-2xl px-4 py-16 sm:px-6 lg:px-8 ">
-          <div className="grid grid-cols-1 lg:h-screen lg:grid-cols-2">
+          <div className="grid grid-cols-1 lg:grid-cols-2">
             <div className="relative z-10 lg:py-16 ">
               <div className="relative h-64 sm:h-80 lg:h-full">
                 <img
